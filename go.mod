4e584fbd87e32c086f0e7c2a18bef5bffe886a7f
yanlele <331393627@qq.com>
2021-02-15 00:13:02 +0800

    feat: 完整的静态文件上传过程

--- go.mod
+++ go.mod
@@ -11,6 +11,8 @@ require (
 	github.com/levigross/grequests v0.0.0-20190908174114-253788527a1a
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/tidwall/gjson v1.6.8
+	github.com/tidwall/pretty v1.0.5 // indirect
 	github.com/ugorji/go v1.2.4 // indirect
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
 	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
--- src/controller/upload.go
+++ src/controller/upload.go
@@ -2,18 +2,55 @@ package controller
 
 import (
 	"encoding/base64"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/levigross/grequests"
+	"github.com/tidwall/gjson"
 	"net/http"
+	"time"
 )
 
 func Upload(c *gin.Context) {
 	fileHeader, _ := c.FormFile("file")
 	file, _ := fileHeader.Open()
 
-	dist := make([]byte, 50000000)                        //开辟存储空间
+	dist := make([]byte, 50000000) //开辟存储空间
 	n, _ := file.Read(dist)
 	sourceString := base64.StdEncoding.EncodeToString(dist[:n])
-	c.JSON(http.StatusOK, gin.H{
-		"base64": sourceString,
+	//c.JSON(http.StatusOK, gin.H{
+	//	"base64": sourceString,
+	//})
+
+	fileName := fmt.Sprintf("%d-%s", time.Now().UnixNano()/1e6, fileHeader.Filename)
+
+	// 请求
+	requestUrl := fmt.Sprintf(
+		"%s%s",
+		"https://gitee.com/api/v5/repos/yanleweb/static/contents/hd_client/",
+		fileName,
+	)
+	//	https://gitee.com/api/v5/repos/yanleweb/static/contents/hd_client/demo-2.jpg
+	res, err := grequests.Post(requestUrl, &grequests.RequestOptions{
+		Headers: map[string]string{
+			"Content-Length": "multipart/form-data; boundary=<calculated when request is sent>",
+		},
+		Data: map[string]string{
+			"access_token": "7a6fab06411e866dcf4c722223f79495",
+			"message":      fileName,
+			"content":      sourceString,
+		},
 	})
+
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"base64":       sourceString,
+		"download_url": gjson.Get(res.String(), "content.download_url").String(),
+	})
+
 }
